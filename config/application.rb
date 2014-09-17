e7e8e7cd0d1dcb63cc590982dcd37127fe3025ea
koudaiii <cs006061@gmail.com>
2014-09-18 00:59:19 +0900

    # Precompile additional assets   config.assets.precompile += %w(admin.js admin.css flatly.js flatly.css)

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module TwitterBootswatchRailsDemo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # Precompile additional assets
+    config.assets.precompile += %w(admin.js admin.css flatly.js flatly.css)
   end
 end
